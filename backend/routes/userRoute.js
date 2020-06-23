1b94a42260b73e7f4453bafd1cdbf1199dbaae90
unknown <rmzslfv1@abv.bg>
2020-06-23 13:51:59 +0300

    Register added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/userModel";
+import { getToken } from "../util";
 
 const router = express.Router();
 
@@ -18,6 +19,27 @@ router.post("/signin", async (req, res) => {
     });
   } else {
     res.status(401).send({ msg: "Invalid Email or Password." });
+  }
+});
+
+router.post("/register", async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  const newUser = await user.save();
+
+  if (newUser) {
+    res.send({
+      _id: newUser.id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: getToken(newUser),
+    });
+  } else {
+    res.status(401).send({ msg: "Invalid User Data." });
   }
 });
 
